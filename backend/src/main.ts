761ea00e635f50b85f976a65ded88706503d29f3
Shmuel Bisberg <shmuelbisberg.acc@gmail.com>
2025-05-25 18:25:57 +0300

    Enhance main application setup: add JWT authentication guard and update server port configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalGuards(new JwtAuthGuard(app.get(Reflector)));
+  await app.listen(process.env.BACKEND_PORT ?? 8080);
 }
-bootstrap();
+
+void bootstrap();
